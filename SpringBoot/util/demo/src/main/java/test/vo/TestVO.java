1a0deca8a4c2895c738e692a44fec6cfca456e16
cdnv1 <tjshi987@naver.com>
2024-07-31 17:14:38 +0900

    20240731 seo

--- SpringBoot/util/demo/src/main/java/test/vo/TestVO.java
+++
@@ -1,8 +0,0 @@
-package test.vo;
-
-import lombok.Data;
-
-@Data //Getter, Setter 만들어줌 (연산을 너무 많이 수행해서 앞으로 사용 X)
-public class TestVO {
-    private String id, name;
-}
--- SpringBoot/util/ex0725_db/src/main/java/com/sist/ex0725_db/mapper/DeptMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sist.ex0725_db.mapper;
-
-import java.util.List;
-
-import com.sist.ex0725_db.vo.DeptVO;
-
-public interface DeptMapper {
-    List<DeptVO> all();
-}
